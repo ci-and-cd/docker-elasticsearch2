35ae159c4982d4a59ed72cc82318646ed0075915
haolun zhang <opensource@infra.top>
2018-09-06 20:23:28 +0800

    travis-ci build

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker version
+docker-compose version
+
+WORK_DIR=$(pwd)
+
+if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
+
+export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
+export IMAGE_NAME=${IMAGE_NAME:-elasticsearch2}
+export IMAGE_TAG=${IMAGE_ARG_IMAGE_TAG:-2.4.4}
+if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
+
+# Build image
+if [[ "$(docker images -q ${IMAGE_PREFIX}/${IMAGE_NAME}:${IMAGE_TAG} 2> /dev/null)" == "" ]]; then
+    docker-compose build elasticsearch2
+fi
+
+docker-compose push
